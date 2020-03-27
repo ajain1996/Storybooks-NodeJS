a115ea13e7ef2fd7ed32be5b7398159718eaf6c2
Ankit Jain <ankitjain.aj1996@google.com>
2020-03-27 12:48:10 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
+
+//passport config
+require('./config/passport')(passport);
+
+// Load routes
+const auth = require('./routes/auth');
 
 app.get('/', function(req, res){
     res.send('This is start app');
 });
+
+// Use routes
+app.use('/auth', auth);
 
 app.listen(8000, function(){
     console.log('Server is listening on port 8000');
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: '',
+    googleClientID: '494546554208-bdk6tovd18ok5l4go6ee1arv1k03pr3l.apps.googleusercontent.com',
+    googleClientSecret: '8rJDRUj3Z3qDIh1QYvkpBa4Z',
+}
